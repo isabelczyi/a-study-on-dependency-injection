43853611ecf075bf31227d40b012bf5c9f9a29e8
Isabel Cheung <isabelczyi@gmail.com>
2022-07-04 23:36:51 +1000

    holy shit no errors

--- src/cpu/cpu.module.ts
+++ src/cpu/cpu.module.ts
@@ -5,5 +5,6 @@ import { PowerModule } from 'src/power/power.module';
 @Module({
   imports: [PowerModule], //step 2 add in the power module in the cpu module
   providers: [CpuService],
+  exports: [CpuService],
 })
 export class CpuModule {}
--- src/cpu/cpu.service.ts
+++ src/cpu/cpu.service.ts
@@ -5,7 +5,7 @@ import { PowerService } from 'src/power/power.service';
 export class CpuService {
   constructor(private powerService: PowerService) {}
 
-  computer(a: number, b: number) {
+  compute(a: number, b: number) {
     this.powerService.supplyPower(10); //just an example to make use of the service in some way
     return a + b;
   }
