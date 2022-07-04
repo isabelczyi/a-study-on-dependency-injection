b40a8f75411a3e18ec0414ce5e4a8671455ef73b
Isabel Cheung <isabelczyi@gmail.com>
2022-07-04 23:27:05 +1000

    using the powerservice in the cpu service

--- src/cpu/cpu.service.ts
+++ src/cpu/cpu.service.ts
@@ -4,4 +4,9 @@ import { PowerService } from 'src/power/power.service';
 @Injectable()
 export class CpuService {
   constructor(private powerService: PowerService) {}
+
+  computer(a: number, b: number) {
+    this.powerService.supplyPower(10); //just an example to make use of the service in some way
+    return a + b;
+  }
 }
